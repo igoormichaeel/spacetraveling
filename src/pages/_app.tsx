b082b9b6bdffc8aad42049e9711cfdfddac46a2c
IgorMichael <igormichael120@gmail.com>
2022-02-16 21:31:06 -0300

    BarProgress added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import Header from '../components/Header';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <NextNProgress
+        color="#ff57b2"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
       <Header />
       <Component {...pageProps} />
     </>
